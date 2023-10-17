89ab3c87f8fca3e050b142d55401b0a0b0cff9f3
AakashQMP <aakash@qmpglobal.in>
2023-10-17 17:26:21 +0530

    Notification Screen

--- lib/screens/notification/notification_screen.dart
+++ lib/screens/notification/notification_screen.dart
@@ -11,27 +11,25 @@ class NotificationScreen extends StatefulWidget {
 class _NotificationScreenState extends State<NotificationScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Notifications'),
-      ),
-      body: FutureBuilder(
-        future: fetchNotifications(), // Create this function to fetch data
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return CircularProgressIndicator();
-          } else if (snapshot.hasError) {
-            return Text('Error: ${snapshot.error}');
-          } else {
-            // Display the list of notifications
-            return buildNotificationList(snapshot.data);
-          }
-        },
+    return SafeArea(
+      child: Scaffold(
+        body: FutureBuilder(
+          future: fetchNotifications(), // Create this function to fetch data
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+            } else if (snapshot.hasError) {
+              return Text('Error: ${snapshot.error}');
+            } else {
+              // Display the list of notifications
+              return buildNotificationList(snapshot.data);
+            }
+          },
+        ),
       ),
     );
   }
 
-  // Define a function to fetch notifications from Firestore
   Future<List<Map<String, dynamic>>?> fetchNotifications() async {
     try {
       final userUID = currentUser!.uid;
@@ -39,7 +37,7 @@ class _NotificationScreenState extends State<NotificationScreen> {
       final querySnapshot = await firestore
           .collection('Notification')
           .where('uid', isEqualTo: userUID)
-          .orderBy("timestamp", descending: true)
+          // .orderBy("timestamp", descending: true)
           .get();
 
       return querySnapshot.docs
@@ -51,37 +49,125 @@ class _NotificationScreenState extends State<NotificationScreen> {
     }
   }
 
-  // Build the list of notifications
+  Future<void> deleteAllNotifications() async {
+    try {
+      final userUID = currentUser!.uid;
+      final firestore = FirebaseFirestore.instance;
+      final querySnapshot = await firestore
+          .collection('Notification')
+          .where('uid', isEqualTo: userUID)
+          .get();
+
+      for (final document in querySnapshot.docs) {
+        await document.reference.delete();
+      }
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Notifications have been cleared.'),
+        ),
+      );
+
+      setState(() {});
+    } catch (e) {
+      print('Error deleting notifications: $e');
+    }
+  }
 
   Widget buildNotificationList(List<Map<String, dynamic>>? notifications) {
-    if (notifications == null || notifications.isEmpty) {
-      return Center(child: Text('No notifications available.'));
-    } else {
-      return ListView.builder(
-        itemCount: notifications.length,
-        itemBuilder: (context, index) {
-          final notification = notifications[index];
-          final timestamp = notification['timestamp'] as Timestamp;
+    Size size = MediaQuery.sizeOf(context);
 
-          final formattedDate =
-              DateFormat('dd MMMM').format(timestamp.toDate());
-          final formattedTime =
-              DateFormat('hh:mm a').format(timestamp.toDate());
-          final date = formattedDate + " ${formattedTime}";
+    return Padding(
+      padding: EdgeInsets.all(size.width / 16),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          InkWell(
+              onTap: () {
+                Navigator.pop(context);
+              },
+              child: Icon(Icons.arrow_back_ios)),
+          SizedBox(
+            height: 15,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                "Notifications",
+                style: TextStyle(fontSize: 28, fontWeight: FontWeight.w700),
+              ),
+              InkWell(
+                onTap: () {
+                  deleteAllNotifications();
+                },
+                child: Container(
+                  width: size.width * 0.13,
+                  height: size.width * 0.13,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.blue.withOpacity(0.7),
+                  ),
+                  child: Center(
+                    child: Icon(
+                      Icons.delete,
+                      color: Colors.white,
+                      size: size.width * 0.06,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          (notifications == null || notifications.isEmpty)
+              ? Padding(
+                  padding: EdgeInsets.only(top: size.width * 0.75),
+                  child:
+                      Text("Looks like you haven't recieved any notification"),
+                )
+              : ListView.separated(
+                  separatorBuilder: (context, index) => Divider(),
+                  shrinkWrap: true,
+                  itemCount: notifications.length,
+                  itemBuilder: (context, index) {
+                    final notification = notifications[index];
+                    final timestamp = notification['timestamp'] as Timestamp;
 
-          return ListTile(
-            title: Text(notification['title']),
-            subtitle: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(notification['body']),
-                Text(date),
-              ],
-            ),
-            // You can display additional notification details here
-          );
-        },
-      );
-    }
+                    final formattedDate =
+                        DateFormat('dd MMMM').format(timestamp.toDate());
+                    final formattedTime =
+                        DateFormat('hh:mm a').format(timestamp.toDate());
+                    final date = formattedDate + " ${formattedTime}";
+
+                    return ListTile(
+                      leading: InkWell(
+                        onTap: () {},
+                        child: Container(
+                          width: size.width * 0.15,
+                          height: size.width * 0.15,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Color(0xFFd3f5d6),
+                          ),
+                          child: Center(
+                            child: Image.asset("assets/images/call.png"),
+                          ),
+                        ),
+                      ),
+                      title: Text(notification['title']),
+                      subtitle: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(notification['body']),
+                          //Text(date),
+                        ],
+                      ),
+                    );
+                  },
+                )
+        ],
+      ),
+    );
   }
 }
