852bc4cc0608d2913769703a9102067dd38953fb
VinayaAugusthy <vinayaaugusthy@gmail.com>
2023-11-16 11:58:55 +0530

    format

--- lib/functions/contact function/contact_fuctions.dart
+++ lib/functions/contact function/contact_fuctions.dart
@@ -1,16 +1,15 @@
-
 import 'package:flutter/material.dart';
 import 'package:sitare_astrologer_partner/model/user_model.dart';
 import 'package:sitare_astrologer_partner/screens/chat%20screen/service/chat_service.dart';
 
-void sendMessage(TextEditingController controller,ChatService chatService,UserModel user) async {
-    if (controller.text.isNotEmpty) {
-      await chatService.sendMessage(
-          user.uid, controller.text);
+void sendMessage(TextEditingController controller, ChatService chatService,
+    UserModel user) async {
+  if (controller.text.isNotEmpty) {
+    await chatService.sendMessage(user.uid, controller.text);
 
-      controller.clear();
-    }
+    controller.clear();
   }
+}
 
 // FirebaseFirestore firestore = FirebaseFirestore.instance;
 //   Future<List<UserModel>> fetchuserdetailsDromfirestore() async {
