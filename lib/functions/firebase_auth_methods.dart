0081b705654b9fa419ae02f7633c478b69a82d47
AthulprakashQMP <athulprakash@qmpglobal.in>
2023-11-29 17:53:25 +0530

    commented unwanted code

--- lib/functions/firebase_auth_methods.dart
+++ lib/functions/firebase_auth_methods.dart
@@ -7,33 +7,33 @@ final _auth = FirebaseAuth.instance;
 
 User? get currentUser => _auth.currentUser;
 Stream<User?> get authState => _auth.authStateChanges();
-Future<String?> signUpWithEmail({
-  required String email,
-  required String password,
-}) async {
-  try {
-    await _auth
-        .createUserWithEmailAndPassword(
-          email: email,
-          password: password,
-        );
-        return null;
-  } on FirebaseAuthException catch (e) {
-    return e.message.toString();
-  }
-}
+// Future<String?> signUpWithEmail({
+//   required String email,
+//   required String password,
+// }) async {
+//   try {
+//     await _auth
+//         .createUserWithEmailAndPassword(
+//           email: email,
+//           password: password,
+//         );
+//         return null;
+//   } on FirebaseAuthException catch (e) {
+//     return e.message.toString();
+//   }
+// }
 
-Future<String?> loginWithEmail({
-  required String email,required String password
-}) async{
-  try {
-    await _auth.signInWithEmailAndPassword(email: email, password: password);
-    return null;
-  }on FirebaseAuthException catch (e) {
-    return e.message.toString();
+// Future<String?> loginWithEmail({
+//   required String email,required String password
+// }) async{
+//   try {
+//     await _auth.signInWithEmailAndPassword(email: email, password: password);
+//     return null;
+//   }on FirebaseAuthException catch (e) {
+//     return e.message.toString();
     
-  }
-}
+//   }
+// }
 
 
 String verifyId = '';
