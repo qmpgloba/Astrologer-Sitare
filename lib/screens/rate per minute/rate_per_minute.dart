51cfe0d6ef908f918f3bbc0956ca936e75488989
VinayaAugusthy <vinayaaugusthy@gmail.com>
2023-12-04 17:44:14 +0530

    text field added

--- lib/screens/rate per minute/rate_per_minute.dart
+++ lib/screens/rate per minute/rate_per_minute.dart
@@ -28,12 +28,32 @@ class _RatePerMinuteScreenState extends State<RatePerMinuteScreen> {
         child: Padding(
           padding: EdgeInsets.all(size.width * 0.1),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
             children: [
+              SizedBox(
+                height: size.width * 0.1,
+              ),
+              const Text(
+                'SITARE',
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  color: blackColor,
+                ),
+              ),
+              SizedBox(
+                height: size.width * 0.2,
+              ),
+              const Text(
+                'Enter rate per minute(between 0-10)',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: size.width * 0.05,
+              ),
               TextFormField(
                 controller: rateController,
                 decoration: InputDecoration(
-                  labelText: 'Enter the rate per minute(between 0-10)',
+                  hintText: 'Enter rate here...',
                   border: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(16),
                   ),
@@ -41,7 +61,7 @@ class _RatePerMinuteScreenState extends State<RatePerMinuteScreen> {
                 keyboardType: TextInputType.number,
               ),
               const SizedBox(
-                height: 10,
+                height: 30,
               ),
               ElevatedButton(
                 onPressed: () => onTapConfirm(rateController.text),
