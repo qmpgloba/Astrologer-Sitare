7c0453d53e1c5cc30be20375d24d3e053ac9c6a4
VinayaAugusthy <vinayaaugusthy@gmail.com>
2023-12-04 15:40:36 +0530

    textform field added

--- lib/screens/rate per minute/rate_per_minute.dart
+++ lib/screens/rate per minute/rate_per_minute.dart
@@ -8,10 +8,34 @@ class RatePerMinuteScreen extends StatefulWidget {
 }
 
 class _RatePerMinuteScreenState extends State<RatePerMinuteScreen> {
+  final TextEditingController rateController = TextEditingController();
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.sizeOf(context);
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: const Text('Rate Per Minute'),
+        centerTitle: true,
+      ),
+      body: SafeArea(
+          child: Padding(
+        padding: EdgeInsets.all(size.width * 0.1),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextFormField(
+              controller: rateController,
+              decoration: InputDecoration(
+                labelText: 'Enter the rate per minute(between 0-10)',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(16),
+                ),
+              ),
+              keyboardType: TextInputType.number,
+            )
+          ],
+        ),
+      )),
     );
   }
 }
