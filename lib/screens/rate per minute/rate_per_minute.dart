06769bad12ad0765b4e6d9c12bded4384a8a0b4e
VinayaAugusthy <vinayaaugusthy@gmail.com>
2023-12-04 14:27:47 +0530

    RatePerMinuteScreen created

---
+++ lib/screens/rate per minute/rate_per_minute.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RatePerMinuteScreen extends StatefulWidget {
+  const RatePerMinuteScreen({super.key});
+
+  @override
+  State<RatePerMinuteScreen> createState() => _RatePerMinuteScreenState();
+}
+
+class _RatePerMinuteScreenState extends State<RatePerMinuteScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
