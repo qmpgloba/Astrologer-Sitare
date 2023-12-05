dcb7c77766f55065d15c129a8a38413669bf65e9
VinayaAugusthy <vinayaaugusthy@gmail.com>
2023-12-05 12:00:48 +0530

    web option added

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:cron/cron.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:sitare_astrologer_partner/constants/ui_constants.dart';
@@ -39,7 +40,19 @@ Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+          apiKey: "AIzaSyCyY2g27oNebbND84uPRKvx8MwrfZziKC4",
+          authDomain: "sitare-afebd.firebaseapp.com",
+          projectId: "sitare-afebd",
+          storageBucket: "sitare-afebd.appspot.com",
+          messagingSenderId: "1090327514590",
+          appId: "1:1090327514590:web:51ffb157f87d5cc23d214f"),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
   tzdata.initializeTimeZones();
   tz.setLocalLocation(tz.getLocation('Asia/Kolkata'));
   await _initFCM();
